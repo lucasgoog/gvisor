1f684c85b7091e063561077501ed55b8013920d0
Ayush Ranjan <ayushranjan@google.com>
2023-12-12 10:37:11 -0800

    Add GPU checkpoint/restore test.
    Note that GPU state is not restored. This tests that the sandbox is restored
    and the GPUs are accessible and functional after restore.
    
    Updates #9363, #9649, #9767
    
    PiperOrigin-RevId: 590256769

--- test/gpu/gpu_smoke_test.go
+++ test/gpu/gpu_smoke_test.go
@@ -18,8 +18,10 @@ package gpu_smoke_test
 import (
 	"context"
 	"testing"
+	"time"
 
 	"gvisor.dev/gvisor/pkg/test/dockerutil"
+	"gvisor.dev/gvisor/pkg/test/testutil"
 )
 
 func TestGPUHello(t *testing.T) {
@@ -34,6 +36,47 @@ func TestGPUHello(t *testing.T) {
 		t.Fatalf("could not run cuda-vector-add: %v", err)
 	}
 	t.Logf("cuda-vector-add output: %s", string(out))
+}
+
+func TestGPUCheckpointRestore(t *testing.T) {
+	if !testutil.IsCheckpointSupported() {
+		t.Skip("Checkpoint is not supported.")
+	}
+	dockerutil.EnsureDockerExperimentalEnabled()
+
+	ctx := context.Background()
+	c := dockerutil.MakeContainer(ctx, t)
+	defer c.CleanUp(ctx)
+
+	opts := dockerutil.GPURunOpts()
+	opts.Image = "basic/cuda-vector-add"
+	if err := c.Spawn(ctx, opts, "sleep", "infinity"); err != nil {
+		t.Fatalf("could not run cuda-vector-add: %v", err)
+	}
+
+	// Run the vector add program.
+	if _, err := c.Exec(ctx, dockerutil.ExecOpts{}, "/bin/sh", "-c", "./vectorAdd"); err != nil {
+		t.Fatalf("docker exec failed: %v", err)
+	}
+
+	// Create a snapshot.
+	if err := c.Checkpoint(ctx, "test"); err != nil {
+		t.Fatalf("docker checkpoint failed: %v", err)
+	}
+	if err := c.WaitTimeout(ctx, time.Minute); err != nil {
+		t.Fatalf("wait failed: %v", err)
+	}
+
+	// Restore the snapshot.
+	// TODO(b/143498576): Remove Poll after github.com/moby/moby/issues/38963 is fixed.
+	if err := testutil.Poll(func() error { return c.Restore(ctx, "test") }, time.Minute); err != nil {
+		t.Fatalf("docker restore failed: %v", err)
+	}
+
+	// Run the vector add program again to ensure GPUs are functional.
+	if _, err := c.Exec(ctx, dockerutil.ExecOpts{}, "/bin/sh", "-c", "./vectorAdd"); err != nil {
+		t.Fatalf("docker exec failed: %v", err)
+	}
 }
 
 func TestCUDATests(t *testing.T) {
